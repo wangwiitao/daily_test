341a0da27083306574463100a6d8fcebcfb64563
975569220@qq.com <975569220@qq.com>
2026-02-04 07:37:51 +0800

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,37 @@
 import { useState } from "react";
-import { createPortal } from "react-dom";
+import { CustomModal } from "./CustomModal";
+import { DialogModal } from "./DialogModal";
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  function setClose(e) {
-    e.stopPropagation();
-    setIsOpen(false);
-  }
+  const [isCustomModalOpen, setIsCustomModalOpen] = useState(true);
+  const [isDialogModalOpen, setIsDialogModalOpen] = useState(true);
 
   return (
-    <div
-      onClick={(e) => {
-        e.stopPropagation;
-        console.log("In the div");
-      }}
-      style={{ position: "relative", marginTop: "100px" }}
-    >
-      <h1>App content</h1>
-      <button onClick={() => setIsOpen(true)}>Show Message</button>
-      <AlertMessage isOpen={isOpen} onClose={(e) => setClose(e)}>
-        Secret Message
-        <br />
-        Click To Close
-      </AlertMessage>
+    <div style={{ position: "relative", marginTop: "20px" }}>
+      <button onClick={() => setIsCustomModalOpen(true)}>
+        Show Custome modal
+      </button>
+      <button onClick={() => setIsDialogModalOpen(true)}>
+        Show Dialog modal
+      </button>
+      <CustomModal
+        isOpen={isCustomModalOpen}
+        onClose={() => setIsCustomModalOpen(false)}
+      >
+        <p>
+          This is a <strong>CUSTOM!</strong> modal
+        </p>
+        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
+      </CustomModal>
+      <DialogModal
+        isOpen={isDialogModalOpen}
+        onClose={() => setIsDialogModalOpen(false)}
+      >
+        <p>
+          This is a <strong>Dialog!</strong> modal
+        </p>
+        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
+      </DialogModal>
     </div>
-  );
-}
-
-function AlertMessage({ children, onClose, isOpen }) {
-  if (!isOpen) return null;
-
-  return createPortal(
-    <div
-      onClick={onClose}
-      style={{
-        cursor: "pointer",
-        position: "absolute",
-        top: ".5rem",
-        left: "50%",
-        translate: "-50%",
-        background: "#777",
-        color: "white",
-        borderRadius: ".5rem",
-        padding: ".5rem",
-      }}
-    >
-      {children}
-    </div>,
-    document.body,
   );
 }
