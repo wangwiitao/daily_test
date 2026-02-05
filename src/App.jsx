f1f720bde42ab32343f84283570a9a488c227ae3
975569220@qq.com <975569220@qq.com>
2026-02-05 23:03:10 +0800

    date picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
+import "./styles.css";
+import { DatePicker } from "./DatePicker";
 import { useState } from "react";
-import { CustomModal } from "./CustomModal";
-import { DialogModal } from "./DialogModal";
-
 export default function App() {
-  const [isCustomModalOpen, setIsCustomModalOpen] = useState(true);
-  const [isDialogModalOpen, setIsDialogModalOpen] = useState(true);
-
-  return (
-    <div style={{ position: "relative", marginTop: "20px" }}>
-      <button onClick={() => setIsCustomModalOpen(true)}>
-        Show Custome modal
-      </button>
-      <button onClick={() => setIsDialogModalOpen(true)}>
-        Show Dialog modal
-      </button>
-      <CustomModal
-        isOpen={isCustomModalOpen}
-        onClose={() => setIsCustomModalOpen(false)}
-      >
-        <p>
-          This is a <strong>CUSTOM!</strong> modal
-        </p>
-        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
-      </CustomModal>
-      <DialogModal
-        isOpen={isDialogModalOpen}
-        onClose={() => setIsDialogModalOpen(false)}
-      >
-        <p>
-          This is a <strong>Dialog!</strong> modal
-        </p>
-        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
-      </DialogModal>
-    </div>
-  );
+  const [value, setValue] = useState(new Date());
+  return <DatePicker value={value} onChange={setValue} />;
 }
