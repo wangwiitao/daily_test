9b915f12fd68513831a751643b43e3355a763617
975569220@qq.com <975569220@qq.com>
2026-02-11 23:52:05 +0800

    react.memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
 import { useState } from "react";
-import { CustomModal } from "./CustomModal";
-import { DialogModal } from "./DialogModal";
+import { Counter } from "./Counter";
 
 export default function App() {
-  const [isCustomModalOpen, setIsCustomModalOpen] = useState(true);
-  const [isDialogModalOpen, setIsDialogModalOpen] = useState(true);
-
+  const [name, setName] = useState("");
   return (
-    <div style={{ position: "relative", marginTop: "20px" }}>
-      <button onClick={() => setIsCustomModalOpen(true)}>
-        Show Custome modal
-      </button>
-      <button onClick={() => setIsDialogModalOpen(true)}>
-        Show Dialog modal
-      </button>
-      <CustomModal
-        isOpen={isCustomModalOpen}
-        onClose={() => setIsCustomModalOpen(false)}
-      >
-        <p>
-          This is a <strong>CUSTOM!</strong> modal
-        </p>
-        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
-      </CustomModal>
-      <DialogModal
-        isOpen={isDialogModalOpen}
-        onClose={() => setIsDialogModalOpen(false)}
-      >
-        <p>
-          This is a <strong>Dialog!</strong> modal
-        </p>
-        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
-      </DialogModal>
-    </div>
+    <>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <br />
+      <Counter initialCount={0} />
+    </>
   );
 }
